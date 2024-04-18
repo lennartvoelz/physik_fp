5ac9cef68fd6ca6836ed85696dc0497c3302a299
lennartvoelz <lennart.voelz@tu-dortmund.de>
2024-04-19 00:06:10 +0200

    auswertung fertig (glaub ich)

--- V01/vXXX/lifespan.py
+++ V01/vXXX/lifespan.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 import mca
+import uncertainties as unc
 
 
 counts = np.loadtxt('../data/myonen.txt', unpack=True)
@@ -20,9 +21,9 @@ plt.clf()
 plt.bar(bin_edges[:-1], counts, align="center", yerr=counts_err, capsize=3, ecolor='black', label="Messwerte", edgecolor='black', linewidth=0.7, width=0.44)
 
 def fit_func(x, a, b):
-    return a*np.exp(-b*(x))
+    return a*np.exp(-x/b)
 
-popt, pcov = curve_fit(fit_func, bin_edges[:-1], counts, p0=[1200, 0.1])
+popt, pcov = curve_fit(fit_func, bin_edges[:-1], counts, p0=[800, 2.2])
 
 errors = np.sqrt(np.diag(pcov))
 
@@ -35,6 +36,14 @@ plt.legend(loc='best')
 
 plt.savefig("build/lifespan_hist.pdf")
 
-print("a = ", popt[0], ", b = ", popt[1])
+print("popt: ", popt)
 print("errors: ", errors)
+
+binning_breite = bin_edges[1] - bin_edges[0]
+
+print("Breite der Bins: ", binning_breite)
+
+#b = unc.ufloat(popt[1], errors[1])
+
+
 
